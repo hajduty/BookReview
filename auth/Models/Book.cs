9adf23c6aa655dd7f16cea3f294b5b29626a9e1e
hajder-alr <hajduty@outlook.com>
2024-02-02 23:52:25 +0100

    Populate Book & Review models

--- auth/Models/Book.cs
+++ auth/Models/Book.cs
@@ -2,5 +2,11 @@
 {
     public class Book
     {
+        public string BookID { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public float Rating { get; set; }
+        public string PublicationYear { get; set; }
     }
 }
--- auth/Models/Review.cs
+++ auth/Models/Review.cs
@@ -2,5 +2,11 @@
 {
     public class Review
     {
+        public string ReviewID { get; set; }
+        public string BookID { get; set; }
+        public float Rating { get; set; }
+        public string Description { get; set;}
+        public string UserID { get; set; }
+
     }
 }
