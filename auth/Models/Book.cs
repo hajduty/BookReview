860319d1f28b19ecdc975bb016a6b37f9c036319
hajder-alr <hajduty@outlook.com>
2024-02-03 17:19:07 +0100

    Update book & review models

--- auth/Models/Book.cs
+++ auth/Models/Book.cs
@@ -2,11 +2,11 @@
 {
     public class Book
     {
-        public string BookID { get; set; }
+        public int BookID { get; set; }
         public string Author { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public float Rating { get; set; }
-        public string PublicationYear { get; set; }
+        public int PublicationYear { get; set; }
     }
 }
--- auth/Models/Review.cs
+++ auth/Models/Review.cs
@@ -2,11 +2,10 @@
 {
     public class Review
     {
-        public string ReviewID { get; set; }
-        public string BookID { get; set; }
+        public int ReviewID { get; set; }
+        public int BookID { get; set; }
         public float Rating { get; set; }
         public string Description { get; set;}
         public string UserID { get; set; }
-
     }
 }
